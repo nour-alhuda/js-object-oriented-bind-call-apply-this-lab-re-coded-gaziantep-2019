1bc96f6c15cba23c15f1182cbafaf87845317037
Nour Alhuda Alasali <nouralasali@hotmail.com>
2019-08-18 21:46:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function justInvoke(fn){
 function setThisWithCall(fn, thisValue, arg){
   return fn.call(thisValue, arg);
 }
+function setThisWithApply(fn, thisValue, args){
+  const arr = Array.prototype.slice.call(args);
+  return fn.apply(thisValue, arr);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  const newFunction = functionToBeCopied.bind(thisValue);
+  return newFunction;
+} 
